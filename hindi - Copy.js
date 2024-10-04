fcc230a0d9001f06f7cfc08506a4cb0c99444d10
KhushdilBravo <155454117+KhushdilBravo@users.noreply.github.com>
2024-10-05 00:50:51 +0530

    Add files via upload

---
+++ hindi - Copy.js
@@ -0,0 +1,23 @@
+const btn=document.querySelector(".btn");
+const h1 =document.querySelector(".chatlist")
+const h3=document.querySelector(".container .rightSide")
+const bt=document.querySelector(".bt")
+const ss=document.querySelector(".container")
+const h4=document.querySelector(".kh")
+const h8=document.querySelector(".khs")
+const h9=document.querySelector(".niraz")
+const h10=document.querySelector(".roshan")
+
+
+
+
+    bt.addEventListener("click", function(){
+        // h1.style.backgroundColor="gray"
+        h3.style.backgroundColor="black"
+        ss.style.backgroundColor="black"
+         h1.style.backgroundColor="black"
+        h4.style.color="white"
+        h8.style.color="white"
+        h9.style.color="white"
+        h10.style.color="white"
+    })
